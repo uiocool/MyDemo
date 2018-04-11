3c5878c9094db70d36d50565bcb9f51a81c41b63
uiocool <1429583624@qq.com>
2018-04-12 00:13:21 +0800

    new class

--- app/src/main/java/com/example/administrator/mydemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/mydemo/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import com.example.administrator.mydemo.ui.commend.CommendFragment;
 import com.example.administrator.mydemo.ui.mine.MineFragment;
+import com.example.administrator.mydemo.ui.nearby.NearbyFragment;
 import com.example.administrator.mydemo.ui.plate.PlateFragment;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -19,11 +20,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView txt_test;
     private TextView txt_commend;
     private TextView txt_plate;
+    private TextView txt_nearby;
     private TextView txt_mine;
     private FrameLayout fl_content;
 
     private CommendFragment commend;
     private PlateFragment plate;
+    private NearbyFragment nearby;
     private MineFragment mine;
 
     // 管理器
@@ -42,24 +45,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txt_test = (TextView) findViewById(R.id.txt_test);
         txt_commend = (TextView) findViewById(R.id.txt_commend);
         txt_plate = (TextView) findViewById(R.id.txt_plate);
+        txt_nearby = (TextView)findViewById(R.id.txt_nearby);
         txt_mine = (TextView) findViewById(R.id.txt_mine);
 
         fl_content = (FrameLayout) findViewById(R.id.fl_content);
 
         txt_commend.setOnClickListener(this);
         txt_plate.setOnClickListener(this);
+        txt_nearby.setOnClickListener(this);
         txt_mine.setOnClickListener(this);
     }
 
     private void setSelected(){
         txt_commend.setSelected(false);
         txt_plate.setSelected(false);
+        txt_nearby.setSelected(false);
         txt_mine.setSelected(false);
     }
 
     private void hideAllFragment(FragmentTransaction fragmentTransaction){
         if(commend != null) fragmentTransaction.hide(commend);
         if(plate != null) fragmentTransaction.hide(plate);
+        if(nearby != null) fragmentTransaction.hide(nearby);
         if(mine != null) fragmentTransaction.hide(mine);
     }
 
@@ -85,6 +92,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     fTransaction.add(R.id.fl_content, plate);
                 }
                 fTransaction.show(plate);
+                break;
+            case R.id.txt_nearby:
+                setSelected();
+                txt_nearby.setSelected(true);
+                if(nearby == null){
+                    nearby = new NearbyFragment();
+                    fTransaction.add(R.id.fl_content, nearby);
+                }
+                fTransaction.show(nearby);
                 break;
             case R.id.txt_mine:
                 setSelected();
