68e14b443b622023c1d1f6ad5cc5a7cd81381ef9
uiocool <1429583624@qq.com>
2018-04-30 14:52:38 +0800

    new xml

--- app/src/main/java/com/example/administrator/mydemo/ui/mine/MineFragment.java
+++ app/src/main/java/com/example/administrator/mydemo/ui/mine/MineFragment.java
@@ -1,11 +1,13 @@
 package com.example.administrator.mydemo.ui.mine;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.administrator.mydemo.R;
 
@@ -14,5 +16,19 @@ public class MineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_mine, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        TextView register = (TextView)getView().findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
